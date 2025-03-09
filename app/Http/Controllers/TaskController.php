260e601c29922a1fb9c2116b552638fbf44d1c1a
Marco Custodio <marco.antonio.custodio@gmail.com>
2025-03-09 19:28:12 +0000

    Created boilerplate for controller functions and added relationship between Task and User along with the fillable columns

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index(){
+      $tasks = Task::all();
+
+      return view();
+    }
+
+    public function create(){
+      return view();
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function edit(Task $task){
+      return view();
+    }
+
+    public function update(Request $request, Task $task){
+
+    }
+
+    public function destroy(Task $task){
+
+    }
 }
