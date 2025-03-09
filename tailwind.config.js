0ebd29f6c84648039f5caaad6b75a95e2a6228c1
Marco Custodio <marco.antonio.custodio@gmail.com>
2025-03-09 20:28:00 +0000

    Added brand colours to Tailwind config, cleared welcome page and changed the path of the dashboard page to Tasks

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                "primary" : "#3cb6ce",
+                "accent" : "#92d400"
+            }
         },
     },
 
