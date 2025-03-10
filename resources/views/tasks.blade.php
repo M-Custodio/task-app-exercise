61d8d120b993cbe0a200a2590689dd63d1164167
Marco Custodio <marco.antonio.custodio@gmail.com>
2025-03-10 01:51:49 +0000

    added form and table of tasks

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,20 +1,53 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-black-800 leading-tight">
-            Tasks
-        </h2>
-        <a href="" class="bg-primary hover:bg-accent text-white font-semibold py-2 px-4 rounded shadow">
-            Create Task
-        </a>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-black-800 leading-tight">
+      Tasks
+    </h2>
+    <a href="{{ route('tasks.create') }}"
+      class="bg-primary hover:bg-accent text-white font-semibold py-2 px-4 rounded shadow">
+      Create Task
+    </a>
+  </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-
-                </div>
-            </div>
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900">
+            <table class="table-auto w-full ">
+                <thead>
+                    <tr>
+                        <th>Title/Name</th>
+                        <th>Description</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <td>{{ $task->name }}</td>
+                            <td>{{ $task->description }}</td>
+                            <td class="text-center">{{ $task->status }}</td>
+                            <td class="flex justify-center space-x-7">
+                                <a href="{{ route('tasks.edit', $task->id) }}"
+                                    class="bg-primary hover:bg-accent text-white font-semibold py-2 px-4 rounded shadow">
+                                    Edit
+                                </a>
+                                <form action="{{ route('tasks.destroy', $task->id) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit"
+                                        class="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded shadow">
+                                        Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
+      </div>
     </div>
+  </div>
 </x-app-layout>
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,17 +1,39 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-black-800 leading-tight">
-            Create Task
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-black-800 leading-tight">
+      Create Task
+    </h2>
+  </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900">
+            <form action="{{route('tasks.store')}}" method="post">
+                @csrf
+                @if ($errors->any())
+                    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <div class="mb-4">
+                    <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Title/Name</label>
+                    <input type="text" name="name" id="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                 </div>
-            </div>
+                <div class="mb-4">
+                    <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Description</label>
+                    <textarea name="description" id="description" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
+                </div>
+                <div class="mb-4">
+                    <button type="submit" class="bg-primary hover:bg-accent text-white font-semibold py-2 px-4 rounded shadow">Create Task</button>
+                </div>
+            </form>
         </div>
+      </div>
     </div>
+  </div>
 </x-app-layout>
