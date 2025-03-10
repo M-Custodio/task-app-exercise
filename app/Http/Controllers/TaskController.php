7f020d2481e9be3c14d40b8ff6a03a6d6db15a94
Marco Custodio <marco.antonio.custodio@gmail.com>
2025-03-10 01:54:42 +0000

    Fixed index tasks to show only the logged in user's task.
    Added the return view on create function.
    
    Added the function to store the form request with validation on the name field.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,29 +7,43 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index(){
-      $tasks = Task::all();
+    public function index()
+    {
+        $tasks = Task::where('user_id', auth()->id())->get();
 
-      return view();
+        return view('tasks', compact('tasks'));
     }
 
-    public function create(){
-      return view();
+    public function create()
+    {
+        return view('tasks.create');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|min:3',
+        ]);
 
+        //add status to $request
+        $request->request->add(['status' => 'in-progress']);
+
+        Task::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'status' => $request->status,
+            'user_id' => auth()->id(),
+        ]);
+
+        return redirect()->route('tasks');
     }
 
-    public function edit(Task $task){
-      return view();
+    public function edit(Task $task)
+    {
+        return view();
     }
 
-    public function update(Request $request, Task $task){
+    public function update(Request $request, Task $task) {}
 
-    }
-
-    public function destroy(Task $task){
-
-    }
+    public function destroy(Task $task) {}
 }
